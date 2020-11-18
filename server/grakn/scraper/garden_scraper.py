f0847496ee4249eb9bb95323db0ca5192be93784
Riisnaes <kr378@exeter.ac.uk>
2020-11-18 14:25:44 +0000

    Added a query function ret. subspecies

--- server/grakn/scraper/garden_scraper.py
+++ server/grakn/scraper/garden_scraper.py
@@ -1,20 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
-from json import dump
+import json
 
-import re
-
+# spoof to be allowed access to site
+USR = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'
 URL_norske = "http://norskeplanter.no/nedlasting-av-produktark/planteliste/"
 URL_gardenOrg = "https://garden.org/search/index.php?q="
+PLNTS_NOR = "norwegian_plants.json"
+PLNTS = "plants.json"
+
 #'div', class_="wc-product__part wc-product__description hide-in-grid show-in-list"
 
 def norske_planter(url):
+    plants = {}
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    print(soup)
     results = soup.find(id="site-content")
     plant_elements = results.find_all('div', class_="wc-product-inner")
-    plants = {}
     for plant in plant_elements:
         plant_info = {}
         if plant.h2.a == None:
@@ -27,28 +29,40 @@ def norske_planter(url):
             local_name = str(plant.p)
         plant_info["local_name"] = local_name.split(">")[1].split("<")[0]
         plants[latin_name] = plant_info
-    with open("norwegian_plants.json", "w") as file:
-        dump(plants, file)
+    #plants = json.JSONEncoder().encode(plants)
+    with open(PLNTS_NOR, "w") as file:
+        print("Writing to: " + PLNTS_NOR)
+        json.dump(plants, file)
 
-def gardenOrg_search(plant):
-    #THis could be part of a for loop so that each search is perfomed separately
-    query = URL_gardenOrg + plant
-    #print(query)
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36' # spoof to be allowed access to site
-    page = requests.get(query, headers={'User-agent':user_agent})
+def gardenOrg_search(plant_name):
+    #Finds all plants associated with the plant name and returns a list
+
+    #plants = {}
+    #plants_nor = None
+    #p = {}
+    query = URL_gardenOrg + plant_name.replace(" ", "+")
+    page = requests.get(query, headers={'User-agent':USR})
     soup = BeautifulSoup(page.content, "html.parser")
-    #print(soup.tbody)
     plant_list = soup.tbody.find_all('a')
+    sub_species = []
     for plant in plant_list:
-        #if hasattr(plant, "img"):
-        #    continue
         if "img alt" in str(plant):
             continue
         plant = str(plant).split("\"")[1].split("/")[4]
-        print(plant)
+        sub_species.append(plant)
+    with open(PLNTS_NOR, 'r') as file:
+        plants_nor = json.load(file)
+    #p["sub_species"] = sub_species
+    return sub_species
+    #p["norwegian_name"] = plants_nor[plant_name]["local_name"]
+    #plants[plant_name] = p
+    #plants[plant_name] =
+    with open(PLNTS, 'w') as file:
+        json.dump(plants, file)
+
 
     #plant_list = soup.tbody.find_all(re.compile("\d/([^/]*)"))
 
 #norske_planter(URL_norske)
-gardenOrg_search("Achillea+millefolium")
-gardenOrg_search("Verbascum nigrum")
+q_return = gardenOrg_search("Achillea millefolium")
+#gardenOrg_search("Verbascum nigrum")
