5dd2e336842eecb7c1272dcdcd8d7e088b36ae6c
Riisnaes <kr378@exeter.ac.uk>
2020-11-18 17:01:31 +0000

    Cleaned up and finsihed garden org search function

--- server/grakn/scraper/garden_scraper.py
+++ server/grakn/scraper/garden_scraper.py
@@ -5,7 +5,7 @@ import json
 # spoof to be allowed access to site
 USR = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36'
 URL_norske = "http://norskeplanter.no/nedlasting-av-produktark/planteliste/"
-URL_gardenOrg = "https://garden.org/search/index.php?q="
+URL_gardenOrg = "https://garden.org/"
 PLNTS_NOR = "norwegian_plants.json"
 PLNTS = "plants.json"
 
@@ -40,7 +40,7 @@ def gardenOrg_search(plant_name):
     #plants = {}
     #plants_nor = None
     #p = {}
-    query = URL_gardenOrg + plant_name.replace(" ", "+")
+    query = URL_gardenOrg + "search/index.php?q=" + plant_name.replace(" ", "+")
     page = requests.get(query, headers={'User-agent':USR})
     soup = BeautifulSoup(page.content, "html.parser")
     plant_list = soup.tbody.find_all('a')
@@ -48,8 +48,10 @@ def gardenOrg_search(plant_name):
     for plant in plant_list:
         if "img alt" in str(plant):
             continue
-        plant = str(plant).split("\"")[1].split("/")[4]
-        sub_species.append(plant)
+        print(plant)
+        plant = str(plant).split("\"")
+        link, plant = plant[1], plant[1].split("/")[4].replace("-", " ")
+        sub_species.append({plant:link})
     with open(PLNTS_NOR, 'r') as file:
         plants_nor = json.load(file)
     #p["sub_species"] = sub_species
@@ -57,12 +59,19 @@ def gardenOrg_search(plant_name):
     #p["norwegian_name"] = plants_nor[plant_name]["local_name"]
     #plants[plant_name] = p
     #plants[plant_name] =
-    with open(PLNTS, 'w') as file:
-        json.dump(plants, file)
+    #with open(PLNTS, 'w') as file:
+        #json.dump(plants, file)
 
+def growth_data(link):
+    """Takes a link from the associated with the specific plant from the
+    Garden org website and compiles the growth data into a list"""
+    
+
+    return 0
 
     #plant_list = soup.tbody.find_all(re.compile("\d/([^/]*)"))
 
 #norske_planter(URL_norske)
 q_return = gardenOrg_search("Achillea millefolium")
 #gardenOrg_search("Verbascum nigrum")
+print(q_return)
