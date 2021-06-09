4b5342338a27efa28e6befba329506a8eeb5e075
Riisnaes <kr378@exeter.ac.uk>
2021-06-09 17:25:18 +0100

    fixed a bunch of bugs and implemented enduser

--- init.lua
+++ init.lua
@@ -1,122 +1,40 @@
 local net, wifi, trig, now = net, wifi, gpio.trig, tmr.now
-local EUS_FILE = "wifi_param.lua"
 local TEST, DSLEEP = 4, 5
-local t0, t1, t2 = 0, 0, 0
-local data, shipment = {}, nil
+local t0, t1, t2, t3, t4, t5, p_count = 0, 0, 0, 0, 0, 0, 0
+local data, shipment, DATA_BUFFER = {}, nil, "databuffer"
 local BUTTON = 3 -- Flash button on D3
 local DURATION_FOR_START = 4000000 -- 4 seconds, dsleep duration
-local TIMEOUT_DSLEEP = 5000 -- If not connected by this time - deep sleep
+local TIMEOUT_SERVER_CONNECT = 5000 -- If not connected by this time - deep sleep
 gpio.mode(BUTTON, gpio.INT, gpio.PULLUP)
 local _, reset_reason = node.bootreason()
-local data_sent = 0
-local HOST, PORT, NAME, LOCATION, DURATION_SLEEP, BURST_TRANSFER = dofile("config.lua")
-
-wifi.sta.connect()
-print("Reset reason: "..reset_reason)
-if reset_reason ~= DSLEEP then
-    print("Unexpected reset reason: "..reset_reason)
-    if file.exists("dsleepflag") then
-        print("Removing deep sleep flag")
-        file.remove("dsleepflag")
-    end
+local data_sent = 0 -- 0 = not attempted sent, 1 = data sent, 2 = data attempted sent
+local buffer_count = 0
+local server_port = 1234
+local p = dofile('eus_params.lua')
+local report_interval = (p.report_period * 10000000)/p.m_buffer
+print(report_interval)
+data["chip_info"] = node.info("hw")
+data["Plant Location"] = p.plant_location
+data["Plant Name"] = p.plant_name
+data["Report Period"] = p.report_period
+data["Sensings pr report"] = p.m_buffer
+--local HOST, PORT, NAME, LOCATION, DURATION_SLEEP, BURST_TRANSFER = dofile("config.lua")
+if file.open("counter", "r") then
+    buffer_count = tonumber(file.readline())
+    file.flush()
+    file.close()
 end
-
-local function pressed(level, t, eventcount)
-    --Checking if button has been pressed
-    --If button pressed longer than 4 seconds
-    --the recording loop should start
-    --For now button pressed longer than 4 seconds
-    -- results in recording of sensorvalues
-    if t1 == 0 then 
-        t1 = t
-    else
-        t2 = t
-        t0 = t2 - t1
-        if t0 > DURATION_FOR_START then
-            print("Button pressed for longer than "..DURATION_FOR_START)
-            print("Starting data logging")      
-            send_to_sleep(1000000)
-        end
-        print("Duration: "..t0)
-        t1, t2, t0 = 0, 0, 0
-    end
-end
-
-trig(BUTTON, "both", pressed) -- flash button pressed
-
-wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
-    print("\n\tSTA - GOT IP".."\n\tStation IP: "..T.IP.."\n\tSubnet mask: "..
-    T.netmask.."\n\tGateway IP: "..T.gateway)
-    wifi.eventmon.unregister(wifi.eventmon.STA_GOT_IP)
-    if file.exists("dsleepflag") then
-        print("Deepsleep flag detected, proceeding with logging sequence") 
-        --data["sensors"] = dofile("read_sensors.lua")
-        data["chip_info"] = node.info("hw")
-        data["heap_space"] = node.heap()
-        shipment = sjson.encode(data)
-        print("Connecting to server...")
-        srv = net.createConnection(net.TCP, 0)
-        send_to_sleep(DURATION_SLEEP)
-        srv:on("receive", function(sck, c) print(c) end)
-        srv:on("connection", function(sck, c)
-            print("Now connected")
-            sck:send(shipment)
-        end)
-        srv:on("sent", function(sck, c) 
-            print ("Message Sent")
-            print("Disconnecting wifi..")
-            wifi.sta.disconnect()
-            data_sent = 1
-        end)
-        srv:connect(PORT, HOST)
-    else
-        print("No dsleepflag detected, awaiting further instructions...")
-        print("Disconnecting from wifi to avaoid freeze")
-        wifi.sta.disconnect()
-    end
-end)
-
-function wifi_sta_setup()
-    if wifi.sta.getip() ~= nil then
-        -- if this is the case then should be ready to send data
-        print("Wifi is connected to: "..wifi.sta.gethostname())
-        print("IP: "..wifi.sta.getip())
-    else
-        -- if no ip first check if in station mode
-        if wifi.getmode() == wifi.STATION then
-            print("wifi in station mode") 
-        else
-        -- set to station mode
-            wifi.setmode(wifi.STATION)
-            print("wifi mode changing to station mode")
-        end
-        -- now check if EUS file is present on system
-        if file.exists(EUS_FILE) then
-            local p = dofile(EUS_FILE)
-            local station_cfg={}
-            station_cfg.ssid = p.wifi_ssid
-            station_cfg.pwd = p.wifi_password
-            station_cfg.save = true
-            station_cfg.auto = false
-            print("loading config parameters for wifi connection from EUS file")
-            print("SSID: ".. p.wifi_ssid)
-            print("Saving settings to flash..")
-            wifi.sta.config(station_cfg)
-        else
-        --file does not appear to be present
-            print("No EUS file present, run EUS setup to connect to desired wifi network and run wifi_sta_connect() again..")
-        end
-    end
-end
-
-function send_to_sleep(microsec)
-    tmr.create():alarm(TIMEOUT_DSLEEP, tmr.ALARM_SINGLE, function()
-        if data_sent ~= 1 and file.exists("dsleepflag") then
+wifi.sta.autoconnect(0)
+wifi.sta.disconnect()
+enduser_setup.stop()
+function send_to_sleep(DS_us, timeout_ms)
+    tmr.create():alarm(timeout_ms, tmr.ALARM_SINGLE, function()
+        if data_sent == 2 and file.exists("dsleepflag") then
             print("Timeout on connecting to server")
             print("The data was not sent and will be lost...")
             print("Check if server is running...")
         end
-        print("Going to Sleep for " .. microsec .. " us")
+        print("Going to Sleep for "..DS_us)
         if file.exists("dsleepflag") then
             print("dsleepflag already present")
         else
@@ -127,31 +45,154 @@ function send_to_sleep(microsec)
         end
         --print("dsleep not activated")
         --print("now rebooting to simulate dsleep")
+        print("***********Wifi turning OFF!**********")
         wifi.sta.disconnect()
+        print("**************************************")
         --node.restart()
-        node.dsleep(microsec)
+        node.dsleep(DS_us)
     end)
 end
-
-function sensor_readings_buffer()
-    local f = ""
-    if file.open("counter", "r") then
-        f = tonumber(file.readline())
-        file.close()
-    end
+    
+function buffer_sensor_readings()
     if file.open("counter", "w+") then
-        if f == "" then
+        if buffer_count == 0 then
             file.writeline("1")
+            buffer_count = 1
         else
-            f = f + 1
-            file.writeline(f)
+            buffer_count = buffer_count + 1
+            file.writeline(buffer_count)
         end
+        file.flush()
         file.close()
     end
-    if file.open("data_buffer", "a+") then
-        data["sensors"] = dofile("read_sensors.lua")
+    local tstamp = tmr.now()
+    data["sens_time"] = tstamp
+    data["sensors"] = dofile("read_sensors.lua")
+    if file.open(DATA_BUFFER, "a+") then
         file.writeline(sjson.encode(data))
-        file.close()
+    end
+    file.flush()
+    file.close()
+end
+
+
+
+print("Reset reason: "..reset_reason)
+if reset_reason ~= DSLEEP then
+    print("Unexpected reset reason: "..reset_reason)
+    if file.exists("dsleepflag") then
+        print("Removing deep sleep flag")
+        print("***********Wifi turning OFF!**********")
+        wifi.sta.disconnect()
+        print("**************************************")
+        file.remove("dsleepflag")
+    else
+        print("No Deepsleep flag...")
+        print("***********Wifi turning OFF!**********")
+        wifi.sta.disconnect()
+        print("**************************************")
     end
 end
-        
+
+--print("Checking for Deepsleep flag...")
+if file.exists("dsleepflag") then
+    buffer_sensor_readings()
+    print("Deep sleep flag present")
+    --buffer_sensor_readings()
+    print("Checking Buffer Count. Transfer files at "..p.m_buffer)
+    print("Buffer count currently at: "..buffer_count)
+    if tonumber(buffer_count) >= tonumber(p.m_buffer) then
+        print("Buffer filled - preparing to transfer")
+        print("***********Wifi turning ON!**********")
+        wifi.sta.connect()
+        print("**************************************")
+        print("Resetting buffer counter")
+        file.remove("counter") -- reset buffer counter
+        --file.remove(DATA_BUFFER)
+    else
+        print("Buffer not full, sending to sleep")
+        send_to_sleep(report_interval,4000)      
+    end
+end
+
+
+local function pressed(level, t, eventcount)
+    --Checking if button has been pressed
+    --If button pressed longer than 4 seconds
+    --the recording loop should start
+    
+    --If button is pressed 3 times within a
+    --2 second interval, the device will go into Wifi
+    --STA mode to set reset wifi connection
+    p_count = p_count + 1
+    
+    if t1 == 0 then 
+        t1 = t
+    else
+        t2 = t
+        t0 = t2 - t1
+        if t0 > DURATION_FOR_START then
+            print("Button pressed for longer than "..DURATION_FOR_START)
+            print("Starting data logging")      
+            send_to_sleep(1000000,1)
+        end
+        print("Duration: "..t0)
+        print("Times Button Pressed"..p_count)
+        t1, t2, t0 = 0, 0, 0
+    end
+    
+    if t3 == 0 then
+        t3 = t
+    end
+    if p_count == 6 then
+        -- 6 button edges registered
+        --The button has been pressed three times
+        t4 = t
+        t5 = t4 - t3
+        if t5 < 2000000 then
+            wifi.sta.clearconfig()
+            --The button has been pressed 3 times within two seconds
+            enduser_setup.start("Pion_"..node.chipid())
+            wifi.sta.connect()
+            print("Enduser setup has been initiated...")
+        end
+        t3, t4, t5 = 0, 0, 0
+    end
+end
+
+
+
+trig(BUTTON, "both", pressed) -- flash button pressed
+
+wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
+    print("\n\tSTA - GOT IP".."\n\tStation IP: "..T.IP.."\n\tSubnet mask: "..
+    T.netmask.."\n\tGateway IP: "..T.gateway)
+    wifi.eventmon.unregister(wifi.eventmon.STA_GOT_IP)
+    data["heap_space"] = node.heap()
+    local buffer = nil
+    --shipment = sjson.encode(data)
+    if file.open(DATA_BUFFER) then
+        buffer = sjson.decode(file.read())
+        file.close()
+    end
+    data["readings"] = buffer
+    shipment = sjson.encode(data)   
+    print("Connecting to server...")
+    data_sent = 2 -- data sending attempted
+    srv = net.createConnection(net.TCP, 0)
+    send_to_sleep(report_interval,4000)
+    srv:on("receive", function(sck, c) print(c) end)
+    srv:on("connection", function(sck, c)
+        print("Now connected")
+        sck:send(shipment)
+    end)
+    srv:on("sent", function(sck, c) 
+        print ("Message Sent")
+        print("Disconnecting wifi..")
+        print("***********Wifi turning OFF!**********")
+        wifi.sta.disconnect()
+        print("**************************************")
+        data_sent = 1
+    end)
+    srv:connect(server_port, p.server)
+end)
